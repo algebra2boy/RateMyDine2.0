2d4283efaf5fc27b0d8b90429e52165ff3ed9870
Yongye Tan <yongyetan1209@gmail.com>
2024-01-07 17:15:25 -0500

    add eslint, prettier, dockerfile

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(201).json({ message: "hello" });
-})
+app.get('/', (req, res) => {
+	res.status(201).json({ message: 'hello' });
+});
 
 app.listen(3000, () => {
-    console.log("the server is running on localhost:3000");
+	console.log('the server is running on localhost:3000');
 });

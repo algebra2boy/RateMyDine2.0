96c9af48c998060f349e52d6ca8223eb55d87634
Yongye Tan <yongyetan1209@gmail.com>
2024-01-07 19:11:19 -0500

    update dockerfile and add docker compose

--- api/src/server.ts
+++ api/src/server.ts
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
 	res.status(201).json({ message: 'hello' });
 });
 
-app.listen(3000, () => {
-	console.log('the server is running on localhost:3000');
+app.listen(8080, () => {
+	console.log('the server is running on localhost:8080');
 });

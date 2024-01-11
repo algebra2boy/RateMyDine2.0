a3479ba7a545fb2d2b578e0d8aa128b4e66eeecd
Yongye Tan <yongyetan1209@gmail.com>
2024-01-10 23:07:41 -0500

    Add Zod and middleware

--- api/src/services/auth/auth.routes.ts
+++ api/src/services/auth/auth.routes.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import * as authContoller from './auth.controller.js';
 
+import { ZodMiddleware } from '../../middlewares/ZodValidation.js';
+import { SignUpSchema } from '../../validations/authValidation.js';
+
 const router = express.Router();
 
-router.post('/signup', authContoller.signUp);
+router.post('/signup', ZodMiddleware(SignUpSchema), authContoller.signUp);
 
 export default router;

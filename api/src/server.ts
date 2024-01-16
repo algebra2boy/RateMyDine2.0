134e9780ba25793ef4d44c6f2f151ce386b12a7b
Yongye Tan <yongyetan1209@gmail.com>
2024-01-16 23:27:53 +0800

    Add compression package to compress HTTP response.

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import compression from "compression";
 import 'dotenv/config';
 
 import { MongoDB } from './configs/mongodb.js';
@@ -18,6 +19,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(morganMiddleware);
 app.use(helmet());
+app.use(compression());
 app.use(cors());
 app.use(express.json());
 app.use(errorMiddleware);

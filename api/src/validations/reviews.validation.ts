05887a776663155dc129d820570105753697d3ee
Yongye Tan <yongyetan1209@gmail.com>
2024-01-16 21:22:00 +0800

    update date funciotn and modular the feedback zod object

--- api/src/validations/reviews.validation.ts
+++ api/src/validations/reviews.validation.ts
@@ -1,14 +1,23 @@
-import { z } from "zod";
+import { z, ZodNumber } from 'zod';
+
+const FeedbackCriteria = (feedbackName: string): ZodNumber =>
+    z
+        .number()
+        .int(`${feedbackName} must be an integer`)
+        .min(1, `${feedbackName} should be an integer from 1 to 5`)
+        .max(5, `${feedbackName} should be an integer between 1 to 5`);
 
 export const reviewSchema = z.object({
     body: z.object({
-        FoodQuality: z.string().min(1, 'FoodQuality should be an integer from 1 to 5').max(1, "FoodQuality should be an integer between 1 to 5"),
-        CustomerService: z.string().min(1, 'CustomerService should be an integer from 1 to 5').max(1, "CustomerService should be an integer between 1 to 5"),
-        Atmosphere: z.string().min(1, 'Atmosphere should be an integer from 1 to 5').max(1, "Atmosphere should be an integer between 1 to 5"),
-        Healthiness: z.string().min(1, 'Healthiness should be an integer from 1 to 5').max(1, "Healthiness should be an integer between 1 to 5"),
-        SeatAvailability: z.string().min(1, 'SeatAvailability should be an integer from 1 to 5').max(1, "SeatAvailability should be an integer between 1 to 5"),
-        Taste: z.string().min(1, 'Taste should be an integer from 1 to 5').max(1, "Taste should be an integer between 1 to 5"),
-        // ReviewDescription: z.string().min(1, 'ReviewDescription should not be empty').max(200, "ReviewDescription should be within 200 characters"),
+        foodQuality: FeedbackCriteria('foodQuality'),
+        customerService: FeedbackCriteria('customerService'),
+        atmosphere: FeedbackCriteria('atmosphere'),
+        healthiness: FeedbackCriteria('healthiness'),
+        seatAvailability: FeedbackCriteria('seatAvailability'),
+        taste: FeedbackCriteria('taste'),
+        description: z
+            .string()
+            .min(1, 'description should not be empty')
+            .max(200, 'description should be within 200 characters'),
     }),
 });
-

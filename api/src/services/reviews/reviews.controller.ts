76e6827f28c0acda63714989037b073277dfcf20
Yongye Tan <yongyetan1209@gmail.com>
2024-01-16 22:18:44 +0800

    add more typing to to review controller

--- api/src/services/reviews/reviews.controller.ts
+++ api/src/services/reviews/reviews.controller.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
 import { MongoDB } from '../../configs/mongodb.js';
-import { DiningInfo} from './reviews.model.js';
+import { DiningInfo, Review } from './reviews.model.js';
 import { Collection } from 'mongodb';
-import * as reviewService from "./reviews.service.js"; 
+import * as reviewService from './reviews.service.js';
 
 /**
  * Retrieve dining hall info such as name and count of reviews.
@@ -11,15 +11,15 @@ const getAllDiningInfo = async (req: Request, res: Response) => {
     const collection: Collection<DiningInfo> = MongoDB.getRateMyDineDB().collection('diningInfo');
     const diningInfo: DiningInfo[] = await collection.find({}).toArray();
 
-    res.send(diningInfo);
+    res.status(200).json(diningInfo);
 };
 
 /**
  * retrieve one dining info
  */
 const getDiningInfo = async (req: Request, res: Response) => {
-    // grabs the dining hall name from the URL
-    const diningName = req.params.diningHall;
+    // grabs the dining hall name from the query parameter
+    const diningName: string = req.params.diningHall;
 
     const collection: Collection<DiningInfo> = MongoDB.getRateMyDineDB().collection('diningInfo');
     const diningInfo: DiningInfo | null = await collection.findOne({ name: diningName });
@@ -32,39 +32,28 @@ const getDiningInfo = async (req: Request, res: Response) => {
         });
     }
     res.status(200).json(diningInfo);
-}
+};
 
 /**
  * get all the food review from a particular dining hall
  */
 const getReviewByDiningHall = async (req: Request, res: Response) => {
+    const diningHallName: string = req.params.dininghall;
+    const document: Review[] | undefined = await reviewService.getReview(diningHallName);
 
-    const diningHallName = req.params.dininghall;
-
-    const document = await reviewService.getReview(diningHallName);
-    res.send(document);
-}
+    res.status(200).json(document);
+};
 
 const createReviewForDiningHall = async (req: Request, res: Response) => {
+    const diningHallReview = req.body;
+    const diningHallName: string = req.params.diningHall;
 
-//     console.log(req.isAuthenticated());
-//     if (!req.isAuthenticated()) { // user is not authenticated
-//         res.status(401).json( { message: "user is not authorized to make a review", status : "failure" } );
-//         return;
-//     }
-
-//     // user is authenticated from here
-    const diningHallReview = req.body; // grabs the body from the post requests
-    const diningHallName   = req.params.diningHall;
-
-
-    const result = await reviewService.createReview(diningHallName, diningHallReview, "jcli47") as string;
+    const result = (await reviewService.createReview(
+        diningHallName,
+        diningHallReview,
+        'jcli47',
+    )) as string;
     res.send(result);
-}
+};
 
-export {
-    getAllDiningInfo,
-    getDiningInfo,
-    getReviewByDiningHall,
-    createReviewForDiningHall
-}
+export { getAllDiningInfo, getDiningInfo, getReviewByDiningHall, createReviewForDiningHall };
--- api/src/services/reviews/reviews.routes.ts
+++ api/src/services/reviews/reviews.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as reviewController from "./reviews.controller.js";
+import * as reviewController from './reviews.controller.js';
 import ZodMW from '../../middlewares/ZodValidation.mw.js';
 import { reviewSchema } from '../../validations/reviews.validation.js';
 
@@ -7,8 +7,8 @@ const reviewRouter = express.Router();
 
 reviewRouter.get('/diningInfo', reviewController.getAllDiningInfo);
 reviewRouter.get('/info/:diningHall', reviewController.getDiningInfo);
-reviewRouter.get('/:dininghall', reviewController.getReviewByDiningHall)
-reviewRouter.post("/:diningHall", ZodMW(reviewSchema), reviewController.createReviewForDiningHall);
+reviewRouter.get('/:dininghall', reviewController.getReviewByDiningHall);
+reviewRouter.post('/:diningHall', ZodMW(reviewSchema), reviewController.createReviewForDiningHall);
 
 // // create a new food review for a particular dining hall
 // reviewRouter.post("/review/:diningHall", ValidateFoodReviewSchema,  async (req, res) => {

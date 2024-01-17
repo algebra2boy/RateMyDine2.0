05916f2601e5ee82f084f4fb5fb1913e4d2c1e2a
Yongye Tan <yongyetan1209@gmail.com>
2024-01-18 00:44:45 +0800

    update jest config and found this github issue be useful: https://github.com/kulshekhar/ts-jest/issues/1057

--- api/src/tests/utils.test.ts
+++ api/src/tests/utils.test.ts
@@ -1,5 +1,5 @@
 import { Feedback } from "../app/services/reviews/reviews.model.js";
-import { computeAverageScore } from "../app/utils/computeScore.utils.js";
+import { computeAverageScore } from "../../src/app/utils/computeScore.utils.js";
 
 describe("test compute score utils function", () => {
     it("test feedback with all one star rating", () => {
@@ -12,8 +12,8 @@ describe("test compute score utils function", () => {
             seatAvailability: 1,
             taste: 1
         }
-        // const score: number = computeAverageScore(feedback);
+        const score: number = computeAverageScore(feedback);
 
-        // expect(score).toEqual(1);
+        expect(score).toEqual(1);
     });
 })

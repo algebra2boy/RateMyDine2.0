49a6fda934d5fdb307a5cb6d9227b8861bed1116
Yongye Tan <yongyetan1209@gmail.com>
2024-01-17 22:03:20 +0800

    refactor all the server coce to app folder and add test folder and jest

---
+++ api/src/tests/utils.test.ts
@@ -0,0 +1,19 @@
+import { Feedback } from "../app/services/reviews/reviews.model.js";
+import { computeAverageScore } from "../app/utils/computeScore.utils.js";
+
+describe("test compute score utils function", () => {
+    it("test feedback with all one star rating", () => {
+        const feedback: Feedback = {
+            description: "abc",
+            foodQuality: 1,
+            customerService: 1,
+            atmosphere: 1,
+            healthiness: 1,
+            seatAvailability: 1,
+            taste: 1
+        }
+        // const score: number = computeAverageScore(feedback);
+
+        // expect(score).toEqual(1);
+    });
+})

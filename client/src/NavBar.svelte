159e2210648a11737fd87b5614734e95ca51baf3
Yongye Tan <yongyetan1209@gmail.com>
2024-01-26 00:22:37 +0800

    Add Nav Bar

--- client/src/NavBar.svelte
+++ client/src/NavBar.svelte
@@ -1,30 +1,20 @@
-<script>
-	// import "./app.pcss";
+<script lang="ts">
+	let isCollapsedButtonClicked: boolean = false;
+
+	const toggleCollapsedButton = () => {
+		isCollapsedButtonClicked = !isCollapsedButtonClicked;
+	};
 </script>
 
-<nav class="border-gray-200 bg-white dark:bg-gray-900">
-	<div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
-		<a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-			<img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-			<span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white"
-				>Flowbite</span
-			>
+<nav class="navbar-container maroon">
+	<div class="navbar-row">
+		<a href="/" class="logo-container">
+			<img src="logo.png" class="h-11" alt="RMD Logo" />
+			<span class="nav-title">RateMyDine</span>
 		</a>
-		<button
-			data-collapse-toggle="navbar-default"
-			type="button"
-			class="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-			aria-controls="navbar-default"
-			aria-expanded="false"
-		>
+		<button type="button" class="collpase-btn" on:click={toggleCollapsedButton}>
 			<span class="sr-only">Open main menu</span>
-			<svg
-				class="h-5 w-5"
-				aria-hidden="true"
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 17 14"
-			>
+			<svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
 				<path
 					stroke="currentColor"
 					stroke-linecap="round"
@@ -34,32 +24,13 @@
 				/>
 			</svg>
 		</button>
-		<div class="hidden w-full md:block md:w-auto" id="navbar-default">
-			<ul
-				class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900"
-			>
+		<div class="w-full md:block md:w-auto" class:hidden={isCollapsedButtonClicked}>
+			<ul class="nav-list">
 				<li>
-					<a
-						href="/"
-						class="block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 dark:text-white md:dark:text-blue-500"
-						aria-current="page">Home</a
-					>
+					<a href="/" class="nav-btn maroon">Log in</a>
 				</li>
 				<li>
-					<a
-						href="/"
-						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-						>About</a
-					>
-				</li>
-				<li>
-					<a href="/" class="nav-btn">Services</a>
-				</li>
-				<li>
-					<a href="/" class="nav-btn">Pricing</a>
-				</li>
-				<li>
-					<a href="/" class="nav-btn">Contact</a>
+					<a href="/" class="nav-btn maroon">Sign up</a>
 				</li>
 			</ul>
 		</div>
@@ -67,7 +38,37 @@
 </nav>
 
 <style lang="postcss">
-	.nav-btn{
-		@apply block rounded px-3 py-2 text-gray-900 bg-pink-200 hover:bg-blue-700;
+	.navbar-container {
+		@apply border-gray-200 bg-gray-50 dark:bg-gray-800;
+	}
+
+	.maroon {
+		background-color: maroon;
+	}
+
+	.navbar-row {
+		@apply mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4;
+	}
+
+	.logo-container {
+		@apply flex items-center rtl:space-x-reverse;
+	}
+
+	.nav-list {
+		@apply mt-4 flex flex-col rounded-lg bg-gray-50 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-transparent rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-transparent;
+	}
+
+	.nav-title {
+		@apply self-center whitespace-nowrap text-2xl font-semibold dark:text-white;
+	}
+
+	.nav-btn {
+		@apply block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent
+		 md:hover:text-blue-700 dark:border-gray-700 dark:text-white 
+		 dark:hover:bg-gray-400 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500;
+	}
+
+	.collpase-btn {
+		@apply inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600;
 	}
 </style>
